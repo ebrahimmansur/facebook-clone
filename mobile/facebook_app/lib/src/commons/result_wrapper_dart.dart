7b4b9943bba39569b29f0adff59f9b726a7bcd75
Ebrahim <ebrahimmansur90117@gmail.com>
2022-04-28 13:19:37 +0200

    ⚡️ add result wrapper model
        - helps as a noise cancelling from errors,failuers,execptions

---
+++ mobile/facebook_app/lib/src/commons/result_wrapper_dart.dart
@@ -0,0 +1,14 @@
+class ResultWrapper<TLeft, TRight> {
+  late TLeft _leftType;
+  late TRight _rightType;
+  final bool _isLeftType;
+
+  ResultWrapper.left(TLeft type) : _isLeftType = true;
+  ResultWrapper.right(TRight type) : _isLeftType = false;
+
+  void result(Function(TLeft left) onLeft, Function(TRight right) onRight) =>
+      _isLeftType ? onLeft(_leftType) : onRight(_rightType);
+
+  bool get isLeft => _isLeftType;
+  bool get isRight => _isLeftType != true;
+}
